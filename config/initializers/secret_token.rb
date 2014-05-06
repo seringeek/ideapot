c06a997090359b314a3d45b1a7f49dc49ee3d2a5
seringeek <serinpaul007@gmail.com>
2014-05-06 20:20:34 +0530

    ideapot_start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideapot::Application.config.secret_key_base = '57d823d4354aee8f45f216069dfba35990536c3a4694a44e61233f6b26b4f84effb0daad2729a185ce3cb2adb9327d62b27b1e6a2ae55ab259314bc250a76a3f'
